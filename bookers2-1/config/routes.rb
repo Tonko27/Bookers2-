c833cbb3ed7fefb66e671438dcbd62309b1540e9
Tonko27 <tonkotsu4127@gmail.com>
2021-09-03 10:03:25 +0000

    [Finish]1-3

--- bookers2-1/config/routes.rb
+++ bookers2-1/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites,only: [:create,:destroy]
+  end
 
 end
